4bd960665f59e67750ce440cce5fc067c6a5c60b
mahen <mahenkamail@gmail.com>
2019-12-06 10:33:55 +0000

    fixed and added file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,8 @@
-#modified file 
 public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-	#prints "welocome to git"
-        System.out.println(""welcome to git);
     }
 
 }
