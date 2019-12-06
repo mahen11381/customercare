1a833c0faf62c520d22dcf902b298b92e22ba164
mahen <mahenkamail@gmail.com>
2019-12-06 11:18:48 +0000

    added of part facebbo project

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+	// Prints "welcome to facebook" to the terminal window.
+        System.out.println("facebook");	
     }
 
 }
